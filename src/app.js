c77f07160d5bde2686815cc9a70f060cf48242b7
Dileep A <99489054+dileepsa@users.noreply.github.com>
2022-08-08 15:05:08 +0530

    [#3] dileep/vivek - join a game
    What are the changes?
    
    * Handling join in backend
    * Tested /join
    * Used express session
    
    Why do we need this change?
    
    * To allow user to join a game using valid room id.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,48 @@
 const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+
+const joinHandler = (req, res, next) => {
+  const { roomId } = req.body;
+  const { gameId } = req;
+  if (gameId === +roomId) {
+    res.sendStatus(200);
+    return;
+  }
+  res.sendStatus(400);
+};
+
+const joinLobbyHandler = (req, res) => {
+  res.end('This is the join Lobby');
+};
 
 const createApp = (config) => {
   const app = express();
+  app.use(session({
+    secret: config.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  }));
+  app.use(express.urlencoded({ extended: true }));
+
+  if (config.ENV === 'dev') {
+    app.use(morgan('tiny'));
+  }
+
+  const gameId = 123;
+  app.use((req, res, next) => {
+    req.gameId = gameId;
+    next();
+  });
+
+  app.post('/login', (req, res) => {
+    req.session.username = 'gayatri';
+    res.end();
+  });
+
+  app.post('/join', joinHandler);
+  app.get('/join-lobby', joinLobbyHandler);
+  app.use(express.static(config.PUBLIC));
   return app;
 };
 
