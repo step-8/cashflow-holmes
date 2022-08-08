5203f6744697139c730838085956c81ac36bb203
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-08-08 14:45:31 +0530

    [#11] suresh/govinda - included tests

--- test/superTest/testRegister.js
+++ test/superTest/testRegister.js
@@ -1,5 +1,6 @@
 const { createApp } = require('../../src/app.js');
 const supertest = require('supertest');
+const assert = require('assert');
 
 describe('Register', () => {
   const config = {
@@ -11,10 +12,37 @@ describe('Register', () => {
   };
 
   it('When requested /register GET', (done) => {
-
     supertest(createApp(config))
       .get('/register')
       .expect(/html/)
       .expect(200, done);
+  });
+
+  it('should register the new user /register POST', (done) => {
+    supertest(createApp(config))
+      .post('/register')
+      .send('username=abcd&password=123')
+      .expect('location', '/')
+      .expect(302, done);
+  });
+
+  it('should persist the userdata in the given file', (done) => {
+    const config = {
+      PUBLIC: './public',
+      REGISTER_PAGE: './views/register.html',
+      CRED_PATH: './test/test.json',
+      SECRET: 'test',
+      
+      persistCredentials: (config) => (req, res) => {
+        assert.equal(config.CRED_PATH, './test/test.json');
+        res.redirect('/');
+      }
+    };
+      
+    supertest(createApp(config))
+      .post('/register')
+      .send('username=abcd&password=123')
+      .expect('location', '/')
+      .expect(302, done);
   });
 });
