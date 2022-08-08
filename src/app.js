96a026e552a3f9b15e1eb9bbaf88a2e45767ffaf
SaiCharan A <99522580+saicharanar@users.noreply.github.com>
2022-08-08 15:36:29 +0530

    [#2] sai/gayatri - host game
    What are the changes?
    
    * Added main menu page
    
    Why do we need this change?
    
    * To show main menu page.

--- src/app.js
+++ src/app.js
@@ -7,11 +7,11 @@ const { readCredentials } = require('./helpers/readCredentials.js');
 const { hostHandler } = require('./handlers/hostHandler.js');
 const { serveMainMenu } = require('./handlers/serveMainMenu.js');
 
-
 const createApp = (config) => {
   const app = express();
   readCredentials(config);
 
+  app.use(express.static('public'));
   app.use(morgan('dev'));
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: true }));
@@ -26,12 +26,11 @@ const createApp = (config) => {
   app.use('/register',
     registerRouter(express.Router(), config));
   app.use(express.static(config.PUBLIC));
-  
+
   app.post('/login', (req, res) => {
     req.session.username = 'gayatri';
     res.end();
   });
-
 
   return app;
 };
