b773480d8db6069835419fd91f528e58067f5d7f
Gayatri Vedpathak <99533431+gayatrivedpathak@users.noreply.github.com>
2022-08-11 16:02:14 +0530

    [#16] vivek/gayatri - Highlighting dice for current player
    What are the changes?
    
    * Highlighting dice for current player
    * Allowing current player to roll dice
    
    Why do we need this change?
    
    * To allow current player to roll dice

--- public/js/ratRaceBoard.js
+++ public/js/ratRaceBoard.js
@@ -1,4 +1,30 @@
 (function () {
+  const highlightCurrentPlayer = (game) => {
+    const { username } = game.currentPlayer;
+    const currentPlayerEle = document.querySelector(`#${username}`);
+    currentPlayerEle.style.backgroundColor = 'lightgrey';
+    currentPlayerEle.style.fontWeight = '900';
+    return game;
+  };
+
+  const rollDice = () => {
+    console.log('Rolling Dice');
+    return;
+  };
+
+  const activateDice = (game) => {
+    fetch('/get-user-info')
+      .then(res => res.json())
+      .then(userInfo => {
+        if (userInfo.username === game.currentPlayer.username) {
+          const dice = document.querySelector('#dice-box');
+          dice.style.opacity = 1;
+          dice.style.border = '2px solid black';
+          dice.onclick = rollDice;
+        }
+      });
+  };
+
   const drawPlayers = (game) => {
     const { players } = game;
     fetch('/api/player-info').then(res => res.json()).then(currentPlayer => {
@@ -12,29 +38,22 @@
         if (currentPlayer.username === username) {
           name = username + '(you)';
         }
-        const playerTemplate = ['div', { className: 'row' },
+        const playerTemplate = ['div', { className: 'row', id: username },
           ['div', { className: `${color} icon` }],
           ['div', { className: 'name' }, name]
         ];
         const playerEle = html(playerTemplate);
         playersEle.append(playerEle);
       });
-    });
-    return game;
-  };
-
-  const highlightCurrentPlayer = (game) => {
-    const { username } = game.currentPlayer;
-    const currentPlayerEle = document.querySelector(`#${username}`);
-    currentPlayerEle.style.backgroundColor = 'lightgrey';
-    currentPlayerEle.style.fontWeight = '900';
+    })
+      .then(__ => highlightCurrentPlayer(game))
+      .then(activateDice);
     return game;
   };
 
   const main = () => {
     fetch('/api/game').then(res => res.json())
-      .then(drawPlayers)
-      .then(highlightCurrentPlayer);
+      .then(drawPlayers);
   };
 
   window.onload = main;
