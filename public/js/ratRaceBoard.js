5842bf3d27e7ab9f379377a0dc4160f7ce2ffa81
Gayatri Vedpathak <99533431+gayatrivedpathak@users.noreply.github.com>
2022-08-11 14:58:12 +0530

    [#16] vivek/gayatri - Highlighting current player
    What are the changes?
    
    * Highlighting current player
    
    Why do we need this change?
    
    * So that other players can know whose turn it is

--- public/js/ratRaceBoard.js
+++ public/js/ratRaceBoard.js
@@ -4,18 +4,28 @@
     const playersEle = getElement('#players');
     players.forEach(player => {
       const { username, color } = player;
-      const playerTemplate = ['div', { className: 'row' },
+      const playerTemplate = ['div', { className: 'row', id: username},
         ['div', { className: `${color} icon` }],
         ['div', { className: 'name' }, username]
       ];
       const playerEle = html(playerTemplate);
       playersEle.append(playerEle);
     });
+    return game;
+  };
+
+  const highlightCurrerntPlayer = (game) => {
+    const { username } = game.currentPlayer;
+    const currentPlayerEle = document.querySelector(`#${username}`);
+    currentPlayerEle.style.backgroundColor = 'lightgrey';
+    currentPlayerEle.style.fontWeight = '900';
+    return game;
   };
 
   const main = () => {
-    const url = '/api/game';
-    fetch(url).then(res => res.json()).then(drawPlayers);
+    fetch('/api/game').then(res => res.json())
+      .then(drawPlayers)
+      .then(highlightCurrerntPlayer);
   };
 
   window.onload = main;
