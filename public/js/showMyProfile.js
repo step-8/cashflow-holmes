73ed230ca3f54e5ddb27f8b46109c58e0e8a35cd
Ashritha M <99526903+ashrithamanepalli@users.noreply.github.com>
2022-08-17 17:18:13 +0530

    [#42] ashritha/dileep - Showing expenses and user details in my profile
    What are the changes?
    
    * Changed the z index of the expansion window
    * Created table for expenses
    * Created structure for user details
    
    Why do we need this change?
    
    * To show expenses in my details

--- public/js/showMyProfile.js
+++ public/js/showMyProfile.js
@@ -1,6 +1,52 @@
-const showMyProfile = () => {
+const createExpensesTable = (expenses) => {
+  const wrapper = ['table', {}];
+  const expensesDetails = Object.entries(expenses);
+
+  expensesDetails.forEach(([key, val]) => {
+    const expenseHeader = ['th', {}, key];
+    const expenseValue = ['td', {}, val];
+    const row = ['tr', {}, expenseHeader, expenseValue];
+    wrapper.push(row);
+  });
+
+  return wrapper;
+};
+
+const createMyProfile = (game) => {
+  const { username, color, profile } = game.currentPlayer;
+  const { profession } = game.currentPlayer.profession;
+  createExpensesTable(profile.expenses);
+
   const myProfileTemplate =
     ['div', { id: 'profile', className: 'profile-wrapper' },
+      ['header', {},
+        ['div', { className: 'board' }, 'Rat Race'],
+        ['div', { id: 'my-details' },
+          ['div', { id: 'username' }, username],
+          ['div', { id: 'profession' }, profession],
+          ['div', { className: `icon ${color}` }]
+        ]
+      ],
+      ['main', {},
+        ['div', { id: 'income-statement' },
+          ['h2', {}, 'Income statement',
+            ['div', {},
+              ['div', { className: 'income' },
+                ['h3', {}, 'Income'],
+                ['div', { className: 'salary-wrapper' },
+                  ['div', {}, 'Salary :'],
+                  ['div', {}, profile.income.salary]
+                ],
+                ['h4', {}, 'Real estate']
+              ],
+              ['div', { className: 'expenses' },
+                ['h3', {}, 'Expenses'],
+                createExpensesTable(profile.expenses)
+              ]
+            ],
+          ]
+        ]
+      ],
       ['div', { className: 'close' },
         ['div', {
           className: 'close-btn',
@@ -11,11 +57,22 @@ const showMyProfile = () => {
 
   const myProfile = html(myProfileTemplate);
   const expansionEle = getElement('#expansion-window');
+  expansionEle.style.zIndex = 2;
   expansionEle.replaceChildren('');
   expansionEle.appendChild(myProfile);
+
+  return game;
+};
+
+const showMyProfile = () => {
+  fetch('/api/game')
+    .then(res => res.json())
+    .then(createMyProfile);
 };
 
 const closeMyProfile = (event) => {
+  const expansionEle = getElement('#expansion-window');
+  expansionEle.style.zIndex = -1;
   const profileEle = getElement('#profile');
   profileEle.remove();
 };
