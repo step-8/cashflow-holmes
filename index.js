348b1ced342c5beb2df8e13bc2bb741819317c63
99522580+saicharanar@users.noreply.github.com <saicharan.abbireddy@gmail.com>
2023-01-30 20:31:36 +0530

    Commented Reddis DB unitlization since it is not working, to make the game work using local host

--- index.js
+++ index.js
@@ -13,14 +13,21 @@ const main = () => {
     secret: config.SECRET, resave: false, saveUninitialized: false
   });
 
-  const DB = createClient({ url: URL });
-  DB.on('error', (err) => console.log('DB Client Error', err));
-  DB.connect().then(() => {
-    const app = createApp(config, session, DB);
-    app.listen(PORT,
-      () => console.log(`Server bound to http://localhost:${PORT}`));
-  }
-  );
+  // Reddis DB is porperly working.
+
+  // const DB = createClient({ url: URL });
+  // DB.on('error', (err) => console.log('DB Client Error', err));
+  // DB.connect().then(() => {
+  //   const app = createApp(config, session, DB);
+  //   app.listen(PORT,
+  //     () => console.log(`Server bound to http://localhost:${PORT}`));
+  // }
+  // );
+
+  const app = createApp(config, session);
+  app.listen(PORT, () => {
+    console.log(`Server bound to http://localhost:${PORT}`);
+  });
 };
 
 main();
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -6,8 +6,9 @@ const { playerInfo } = require('../handlers/apiHandlers.js');
 
 const createApiRouter = (DB) => {
   const apiRouter = express.Router();
-  apiRouter.get('/save/:name', saveGameDetails(DB));
-  apiRouter.get('/load/:gameID/:name', loadGameDetails(DB));
+  // No DB
+  // apiRouter.get('/save/:name', saveGameDetails(DB));
+  // apiRouter.get('/load/:gameID/:name', loadGameDetails(DB));
   apiRouter.get('/game', serveGameDetails);
   apiRouter.get('/profession', serveProfession);
   apiRouter.get('/player-info', playerInfo);
